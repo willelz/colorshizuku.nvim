d8b2daee38661ae07f5501b2da9e531c4a99db6e
willelz <37827548+willelz@users.noreply.github.com>
2022-12-12 12:26:01 +0900

    update

---
+++ colors/colorshizuku.lua
@@ -0,0 +1,33 @@
+vim.g.colors_name = 'colorshizuku'
+vim.cmd.highlight('clear')
+vim.o.background = 'light'
+
+local c = {
+  asagao = '#2663a4',
+  ajisai = '#5373ae',
+  tsuyukusa = '#3b91c2',
+  konpeki = '#219dd0',
+  amairo = '#4dbde3',
+  tsukiyo = '#407587',
+  kujaku = '#1d9ea3',
+  shinkai = '#6c88af',
+  syoro = '#388679',
+  shinryoku = '#13815e',
+   chikurin= '#a4b974',
+   fuyusyogun= '#6f8696',
+   kirisame= '#828083',
+   takessumi= '#4e4e4e',
+   tsutsuji= '#d6336c',
+   kosumosu= '#ef829f',
+   momiji= '#de5743',
+   muraskishikibu= '#6c509a',
+   yamabudo= '#8d387c',
+   yuyake= '#f9983a',
+   fuyugaki= '#f06e3a',
+   inaho= '#9b7939',
+   tukushi= '#8c6663',
+   yamaguri= '#56514e',
+}
+
+local syntax = {
+}
